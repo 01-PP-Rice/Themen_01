b8336c490cc38279c10d364eb6c82a51d74b540e
Nguyen Thanh Trung <168724711+Rice-to-meet-you@users.noreply.github.com>
2024-05-08 08:34:44 +0000

    Deklaration + Wertzuweisung

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,4 +1,8 @@
 // console.log("Hi");
 // console.log(Hi);
 let firstName;
-console.log(firstName)
+console.log(firstName);
+firstName = "Max";
+console.log(firstName);
+let familyName = "Mustermann";
+console.log("Hi, ich bin " + firstName + " " + familyName + "!");

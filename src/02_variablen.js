9cff8cd5fde84af374ed9a1fa905561297b6653d
Nguyen Thanh Trung <168724711+Rice-to-meet-you@users.noreply.github.com>
2024-05-08 09:32:25 +0000

    Datentypen

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,8 +1,30 @@
-// console.log("Hi");
-// console.log(Hi);
+/* console.log("Hi");
+console.log(Hi);
 let firstName;
 console.log(firstName);
 firstName = "Max";
 console.log(firstName);
 let familyName = "Mustermann";
 console.log("Hi, ich bin " + firstName + " " + familyName + "!");
+ */
+
+
+let test;
+console.log("Inhalt: " + test);
+console.log("Datentyp: " + typeof test);
+console.log("-----------------------");
+
+test = "hi";
+console.log("Inhalt: " + test);
+console.log("Datentyp: " + typeof test);
+console.log("-----------------------");
+
+test = 2;
+console.log("Inhalt: " + test);
+console.log("Datentyp: " + typeof test);
+console.log("-----------------------");
+
+test = true;
+console.log("Inhalt: " + test);
+console.log("Datentyp: " + typeof test);
+console.log("-----------------------");

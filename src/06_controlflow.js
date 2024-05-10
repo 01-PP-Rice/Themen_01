23ee6cfdf4acf27e064336bdca86621211c40756
Nguyen Thanh Trung <168724711+Rice-to-meet-you@users.noreply.github.com>
2024-05-10 10:44:05 +0000

    06_controlflow.js

---
+++ src/06_controlflow.js
@@ -0,0 +1,34 @@
+// Entscheidungsstrukturen | control flow
+
+// Deklaration + Assignment
+const ageJohn = 30;
+const ageMark = 30;
+
+// Deklaration
+let isJohnOlder, isJohnEqual;
+
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
+
+
+if(isJohnOlder)  
+{
+    console.log("John ist älter!")
+}
+else if(isJohnEqual)
+{
+    console.log("Sie sind gleich alt!")
+}    
+else 
+{
+    console.log("John ist jünger!")
+} 
+

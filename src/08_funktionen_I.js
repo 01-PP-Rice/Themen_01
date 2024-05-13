1d9c9412e9cad989938a5120b24a3507aae319b3
Nguyen Thanh Trung <168724711+Rice-to-meet-you@users.noreply.github.com>
2024-05-13 08:51:55 +0000

    08_funktionen_1.js

---
+++ src/08_funktionen_I.js
@@ -0,0 +1,23 @@
+
+/***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+
+/* test();
+
+function test() 
+{
+    console.log("Hallo Trung"); 
+} */
+
+
+
+
+outputNames();
+
+function outputNames() 
+{
+    let firstName = "Trung"
+    console.log("Hallo, "+firstName+"!");
+
+}

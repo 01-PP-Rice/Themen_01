919d8e0f4d5c34c8b5365eeee2260136bafe4f91
Nguyen Thanh Trung <168724711+Rice-to-meet-you@users.noreply.github.com>
2024-05-13 10:19:19 +0000

    08_funtionen_1.js

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -51,8 +51,24 @@ function outputNames3(firstName, familyName) { // Parameter
     console.log("Hallo, " + firstName + " " + familyName + "!");
 }
 
-
 /***** Funktionen 03a *****/
 // 03a. Vorbereitung -Trennen der Funktionalitäten
 // Postulat: one function = one job (uncle Bob)
 // SRP single responsibility principle
+
+outputNamesSRP("Trung","Nguyen");
+
+function outputNamesSRP(firstName, familyName) { // Parameter
+
+    // 1. Funktionalität: string composing
+    const GAP = " ";
+    let outStr = "Hallo, " + firstName + GAP + familyName + "!"
+
+    // 2. Funktionalität: string output
+    console.log(outStr);
+
+}
+
+
+
+

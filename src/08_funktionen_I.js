5d40368ad79acda0bfbe75be85432bf50504f429
Nguyen Thanh Trung <168724711+Rice-to-meet-you@users.noreply.github.com>
2024-05-13 09:45:20 +0000

    08_funktionen_1.js

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -21,3 +21,22 @@ function outputNames()
     console.log("Hallo, "+firstName+"!");
 
 }
+
+
+outputNames2("Trung");
+
+function outputNames2(firstName) 
+{
+    console.log("Hallo, "+firstName+"!");
+}
+
+
+outputNames3("Trung", "Nguyen");
+
+const prompt = require('prompt-sync')({sigint: true});
+outputNames3(prompt("Vorname?: "), prompt("Nachname?: "));
+
+function outputNames3(firstName, familyName) 
+{
+    console.log("Hallo, "+firstName+" "+familyName+"!");
+}

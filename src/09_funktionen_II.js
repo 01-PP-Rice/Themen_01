90fe24cdb53f823a4278835eb1b7d3e866527a8e
Nguyen Thanh Trung <168724711+Rice-to-meet-you@users.noreply.github.com>
2024-05-14 10:56:33 +0000

    09_funktionen_II.js

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -10,23 +10,57 @@
 4. Ausgabe in Konsole : 
 */
 
-// module: division a / b |  test:
-output(division(4,2));
-output(division(3,2));
-output(division(3,-2));
-output(division(0,2));
-output(division(3,0));
-output(division(0,0));
+const ERROR_STR_GEN = "Irgendwas ist schief gelaufen"
 
+// module: calculator | tests:
+// agreement : "+","-","*",":","/"
+output(calculator(3,2,"+"));
+output(calculator(3,2,"-"));
+output(calculator(3,2,"*"));
+output(calculator(3,2,":"));
+output(calculator(3,2,"/"));
+output(calculator(3,0,"/"));
+output(calculator(3,2,"#?!"));
+
+function calculator(a,b,op){
+
+		switch (op) {
+			case "+":
+				return addition(a,b);
+			case "-":
+				return subtraction(a,b);
+			case "*":
+				return multiplication(a,b);
+			case "/":
+				return division(a,b);
+			default:
+				return ERROR_STR_GEN
+		}
+
+}
+
+
+// module: division a / b |  test:
+// output(division(4,2));
+// output(division(3,2));
+// output(division(3,-2));
+// output(division(0,2));
+// output(division(3,0));
+// output(division(0,0));
 function division(a,b) {
-	return a/b
+
+	if (b == 0) {
+		return "Error: Divided by 0!";
+	} else {
+		return a/b
+	}
+
 }
 
 // module: multiplication a * b |  test:
 // output(multiplication(3,2));
 // output(multiplication(3,-2));
 // output(multiplication(3,0));
-
 function multiplication(a,b) {
 	return a*b
 }
@@ -36,7 +70,6 @@ function multiplication(a,b) {
 // output(subtraction(3,-2));
 // output(subtraction(3,0));
 // output(subtraction(0,2));
-
 function subtraction(a,b) {
 	return a-b;
 }

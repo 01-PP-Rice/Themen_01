cfa07a27ff7248d246bae6715e42a15b5d989982
Nguyen Thanh Trung <168724711+Rice-to-meet-you@users.noreply.github.com>
2024-05-15 08:25:02 +0000

    11_Arraya_I.js

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -6,6 +6,15 @@
  * 
  */
 
+let arr;
+// arr = new Array(); Konstruktor
+// arr = [];
+arr = [2,11,7,8,10] // Array mit Elementen
+
+output(arr);
+output(arr.length); // Anzahl der Elemente
+output(arr[0]); // Index 0 (1. Pos. im Array)
+output(arr[arr.length-1]); // letzte Pos.
 
 
 /**
@@ -18,13 +27,15 @@
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
 // output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-function getSentence(word1,word2,word3,word4) {
+function getSentence(word1,word2,word3,word4, word5, word6) {
     const GAP = " ";
     const PUNCT = ".";
     let str =   word1 + GAP +
                 word2 + GAP +
                 word3 + GAP +
                 word4 + GAP +
+                word5 + GAP +
+                word6 + 
                 PUNCT;
 
     return str;

d4149f51276e41da7b078d9e07ee742a2c0fc4a9
Nguyen Thanh Trung <168724711+Rice-to-meet-you@users.noreply.github.com>
2024-05-16 10:26:16 +0000

    14_Objects.js

---
+++ src/14_objects.js
@@ -0,0 +1,58 @@
+/***** Erinnerung: Arrays *******/
+
+// let arr;
+// arr = ["Ich","bin","Max"];
+// output(arr);
+// output(arr[0]);
+// output("------------");
+
+// // nested arrays - Prinzip: [[[]]]
+// // nested arrays  - hier: [[],[]]
+// arr =   [
+//             ["Ich","bin","Max"],
+//             ["Ich","bin","Moritz"]
+//         ];
+      
+// output(arr);
+// output(arr[0]);
+// output(arr[0][0]+ " " + arr[1][2]);
+// output(arr[1][0]);
+// output("------------");
+
+// // nested loop
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//         output("Index i: " + i + " Index j: " + j);
+//         output(arr[i][j]);       
+//     }
+// }
+
+
+/***** Objekte 1 Daten/Funktionen *******/
+
+let person = {
+                firstName:"Trung",
+                familyName:"Nguyen",
+                salary: [120000, 160000],
+                permission: true
+};
+
+
+// output(person)
+// output(person.firstName);
+// output(person["firstName"]);
+// output(person.familyName);
+// output(person.salary[1]);
+
+const text =  "Ich bin " + person.firstName + 
+              " und verdiene " + person.salary[1] +
+              " p.a.";
+
+ output(text);
+
+
+// Modul: Ausgabe | Test
+//output("hi");
+function output(outputData) {
+    console.log(outputData);
+}
